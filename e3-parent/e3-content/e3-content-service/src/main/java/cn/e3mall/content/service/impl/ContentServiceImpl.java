2bef8de36c4d826123a2afd6ec86490218127214
Mr Xu <385271984@qq.com>
2017-07-18 14:50:36 +0800

    添加redis缓存

--- e3-parent/e3-content/e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentServiceImpl.java
+++ e3-parent/e3-content/e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentServiceImpl.java
@@ -1,11 +1,15 @@
 package cn.e3mall.content.service.impl;
 
+import cn.e3mall.common.jedis.JedisClientPool;
 import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.common.utils.JsonUtils;
 import cn.e3mall.content.service.ContentService;
 import cn.e3mall.mapper.TbContentMapper;
 import cn.e3mall.pojo.TbContent;
 import cn.e3mall.pojo.TbContentExample;
+import com.alibaba.druid.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -20,6 +24,12 @@ public class ContentServiceImpl implements ContentService {
     @Autowired
     TbContentMapper contentMapper;
 
+    @Autowired
+    JedisClientPool jedisClientPool;
+
+    @Value("${CONTENT_LIST}")
+    private String content_list;
+
     @Override
     public E3Result addContent(TbContent content) {
         content.setCreated(new Date());
@@ -31,9 +41,28 @@ public class ContentServiceImpl implements ContentService {
 
     @Override
     public List<TbContent> getContentListByCid(long cid) {
+        //查询缓存
+        try {
+            String objectStr = jedisClientPool.hget(content_list, String.valueOf(cid));
+            if (!StringUtils.isEmpty(objectStr)) {
+                List<TbContent> list = JsonUtils.jsonToList(objectStr, TbContent.class);
+                return list;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         TbContentExample tbContentExample = new TbContentExample();
         TbContentExample.Criteria criteria = tbContentExample.createCriteria();
         criteria.andCategoryIdEqualTo(cid);
-        return contentMapper.selectByExampleWithBLOBs(tbContentExample);
+        List<TbContent> list = contentMapper.selectByExampleWithBLOBs(tbContentExample);
+
+        try {
+            jedisClientPool.hset(content_list, String.valueOf(cid), JsonUtils.objectToJson(list));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 }
